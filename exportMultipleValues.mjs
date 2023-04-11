14a6c82439f0fadc346e36034aa52b1ddb843677
venkatesh <venuamajala16@gmail.com>
2023-04-11 16:55:25 +0000

    5th sol

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,9 @@
+let myArray = ["camel", 265, true, "5.6"];
+let bulb = {
+  watts: 10,
+  type: "LED",
+};
+function multiplyByFour(number) {
+  return 4 * number;
+}
+export { myArray, bulb, multiplyByFour };
